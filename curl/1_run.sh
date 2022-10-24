6bce1ca40e39e0467078ceb30bba2d3ff16617d2
Martino Trevisan <martino.trevisan@polito.it>
2022-10-24 18:36:31 +0200

    Update 1_run.sh

--- curl/1_run.sh
+++ curl/1_run.sh
@@ -2,7 +2,9 @@
 
 
 N=100
-URL="http://tstat.polito.it/tracce/Polito/2015/TCP/log_tcp_complete.xz"
+URL="http://speed.hetzner.de/1GB.bin"
+#URL="http://tstat.polito.it/tracce/Polito/2015/TCP/log_tcp_complete.xz"
+
 OUTFILE="results_pr.txt"
 
 for i in $(seq $N) ; do
